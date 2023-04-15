c07b1e72c6c0a18ef6d5a2ffdd8a39b1ec09b59b
CryS1x <112645431+CryS1x@users.noreply.github.com>
2023-04-16 00:20:50 +0800

    Add files via upload

---
+++ script/script.js
@@ -0,0 +1,19 @@
+if (!localStorage.getItem('animated')) {
+  var originalTitle = document.title;
+  var counter = 0;
+
+  function updateTitle() {
+    document.title = originalTitle.substring(0, counter);
+    if (counter == originalTitle.length) {
+      counter = 0;
+      setTimeout(updateTitle, 1000);
+      document.title = originalTitle;
+      localStorage.setItem('animated', true); // здесь мы используем localStorage вместо sessionStorage
+    } else {
+      counter++;
+      setTimeout(updateTitle, 400);
+    }
+  }
+
+  updateTitle();
+}
